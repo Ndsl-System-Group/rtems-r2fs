690ed7ad0a38282f425539d070dcec6ec6139c0f
DavidingPlus <davidingplus@qq.com>
2026-03-16 06:39:40 +0000

    add TransactionJournalRecord

--- src/journal/journal_processor.c
+++ src/journal/journal_processor.c
@@ -1,1 +1,43 @@
 #include "journal_processor.h"
+
+
+void transactionJournalRecordInit(TransactionJournalRecord *this, uint64_t txId, uint64_t startLpa, uint64_t endLpa)
+{
+    this->txId = txId;
+    this->startLpa = startLpa;
+    this->endLpa = endLpa;
+}
+
+uint64_t transactionJournalRecordGetTxId(TransactionJournalRecord *this)
+{
+    return this->txId;
+}
+
+uint64_t transactionJournalRecordGetStartLpa(TransactionJournalRecord *this)
+{
+    return this->startLpa;
+}
+
+uint64_t transactionJournalRecordGetEndLpa(TransactionJournalRecord *this)
+{
+    return this->endLpa;
+}
+
+bool transactionJournalRecordIsApplied(TransactionJournalRecord *this, uint64_t curHead, uint64_t curTail)
+{
+    if (this->startLpa < this->endLpa)
+    {
+        if (curHead <= curTail)
+        {
+            return curHead >= this->endLpa || curTail < this->startLpa;
+        }
+        else
+        {
+            return curHead >= this->endLpa;
+        }
+    }
+    else
+    {
+        return curHead <= curTail && curHead >= this->endLpa;
+    }
+}
--- src/journal/journal_processor.h
+++ src/journal/journal_processor.h
@@ -1,5 +1,35 @@
 #ifndef _JOURNAL_PROCESSOR_H_
 #define _JOURNAL_PROCESSOR_H_
 
+#include "utils/types.h"
+
+
+struct comm_dev;
+
+
+/**
+ * @brief 事务日志记录，一个对象代表一个事务，记录该事务日志在 SSD 上持久化的 LPA 范围 [startLpa, endLpa)。
+ */
+typedef struct TransactionJournalRecord
+{
+    uint64_t txId;
+    uint64_t startLpa;
+    uint64_t endLpa;
+} TransactionJournalRecord;
+
+
+void transactionJournalRecordInit(TransactionJournalRecord *this, uint64_t txId, uint64_t startLpa, uint64_t endLpa);
+
+uint64_t transactionJournalRecordGetTxId(TransactionJournalRecord *this);
+
+uint64_t transactionJournalRecordGetStartLpa(TransactionJournalRecord *this);
+
+uint64_t transactionJournalRecordGetEndLpa(TransactionJournalRecord *this);
+
+/**
+ * @brief 日志处理线程中，只有在事务日志记录移除后，才释放该事务占用的日志区域资源，增加 curAvailLpa。所以，在此事务日志记录未移除时，curTail 不会二次进入 [startLpa, endLpa) 区域。可以用如下方法判断日志是否已经应用完。
+ */
+bool transactionJournalRecordIsApplied(TransactionJournalRecord *this, uint64_t curHead, uint64_t curTail);
+
 
 #endif
