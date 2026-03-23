d946d78bbc444a9ad5f38fdca7c7269a37292e32
DavidingPlus <davidingplus@qq.com>
2026-03-23 06:22:50 +0000

    rename function names for multithread

--- src/utils/rtfs_multithread.h
+++ src/utils/rtfs_multithread.h
@@ -11,103 +11,43 @@ typedef pthread_cond_t cond_t;
 typedef pthread_rwlock_t rwlock_t;
 
 
-__attribute__((unused)) static int mutex_init(mutex_t *self)
-{
-    return pthread_mutex_init(self, NULL);
-}
+__attribute__((unused)) int rtfsMutexInit(mutex_t *self);
 
-__attribute__((unused)) static int mutex_lock(mutex_t *self)
-{
-    return pthread_mutex_lock(self);
-}
+__attribute__((unused)) int rtfsMutexLock(mutex_t *self);
 
-__attribute__((unused)) static int mutex_trylock(mutex_t *self)
-{
-    return pthread_mutex_trylock(self);
-}
+__attribute__((unused)) int rtfsMutexTrylock(mutex_t *self);
 
-__attribute__((unused)) static int mutex_unlock(mutex_t *self)
-{
-    return pthread_mutex_unlock(self);
-}
+__attribute__((unused)) int rtfsMutexUnlock(mutex_t *self);
 
-__attribute__((unused)) static int mutex_destroy(mutex_t *self)
-{
-    return pthread_mutex_destroy(self);
-}
+__attribute__((unused)) int rtfsMutexDestroy(mutex_t *self);
 
-__attribute__((unused)) static int spin_init(spinlock_t *self)
-{
-    return pthread_spin_init(self, PTHREAD_PROCESS_PRIVATE);
-}
+__attribute__((unused)) int rtfsSpinInit(spinlock_t *self);
 
-__attribute__((unused)) static int spin_lock(spinlock_t *self)
-{
-    return pthread_spin_lock(self);
-}
+__attribute__((unused)) int rtfsSpinLock(spinlock_t *self);
 
-__attribute__((unused)) static int spin_unlock(spinlock_t *self)
-{
-    return pthread_spin_unlock(self);
-}
+__attribute__((unused)) int rtfsSpinUnlock(spinlock_t *self);
 
-__attribute__((unused)) static int spin_destroy(spinlock_t *self)
-{
-    return pthread_spin_destroy(self);
-}
+__attribute__((unused)) int rtfsSpinDestroy(spinlock_t *self);
 
-/* 默认读者优先 */
-__attribute__((unused)) static int rwlock_init(rwlock_t *self)
-{
-    return pthread_rwlock_init(self, NULL);
-}
+__attribute__((unused)) int rtfsRwlockInit(rwlock_t *self);
 
-/* 加共享读锁 */
-__attribute__((unused)) static int rwlock_rdlock(rwlock_t *self)
-{
-    return pthread_rwlock_rdlock(self);
-}
+__attribute__((unused)) int rtfsRwlockRdlock(rwlock_t *self);
 
-/* 加独占写锁 */
-__attribute__((unused)) static int rwlock_wrlock(rwlock_t *self)
-{
-    return pthread_rwlock_wrlock(self);
-}
+__attribute__((unused)) int rtfsRwlockWrlock(rwlock_t *self);
 
-__attribute__((unused)) static int rwlock_unlock(rwlock_t *self)
-{
-    return pthread_rwlock_unlock(self);
-}
+__attribute__((unused)) int rtfsRwlockUnlock(rwlock_t *self);
 
-__attribute__((unused)) static int rwlock_destroy(rwlock_t *self)
-{
-    return pthread_rwlock_destroy(self);
-}
+__attribute__((unused)) int rtfsRwlockDestroy(rwlock_t *self);
 
-__attribute__((unused)) static int cond_init(cond_t *self)
-{
-    return pthread_cond_init(self, NULL);
-}
+__attribute__((unused)) int rtfsCondInit(cond_t *self);
 
-__attribute__((unused)) static int cond_wait(cond_t *self, mutex_t *mtx)
-{
-    return pthread_cond_wait(self, mtx);
-}
+__attribute__((unused)) int rtfsCondWait(cond_t *self, mutex_t *mtx);
 
-__attribute__((unused)) static int cond_signal(cond_t *self)
-{
-    return pthread_cond_signal(self);
-}
+__attribute__((unused)) int rtfsCondSignal(cond_t *self);
 
-__attribute__((unused)) static int cond_broadcast(cond_t *self)
-{
-    return pthread_cond_broadcast(self);
-}
+__attribute__((unused)) int rtfsCondBroadcast(cond_t *self);
 
-__attribute__((unused)) static int cond_destroy(cond_t *self)
-{
-    return pthread_cond_destroy(self);
-}
+__attribute__((unused)) int rtfsCondDestroy(cond_t *self);
 
 
 #endif
