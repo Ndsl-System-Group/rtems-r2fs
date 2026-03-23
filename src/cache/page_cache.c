547f4f7ff6fcbc90cdd13fc0d07333e5b723f791
DavidingPlus <davidingplus@qq.com>
2026-03-23 03:44:06 +0000

    add PageEntry for page_cache

---
+++ src/cache/page_cache.c
@@ -0,0 +1,76 @@
+#include "page_cache.h"
+
+#include "fs/fs.h"
+#include "utils/rtfs_log.h"
+
+
+// 对 isDirty 进行 CAS 操作(由 false 更改为 true)，成功返回 true。
+static bool pageEntryMarkDirty(PageEntry *this);
+
+
+void pageEntryInit(PageEntry *this, uint32_t blkoff)
+{
+    this->blkoff = blkoff;
+    this->lpa = INVALID_LPA;
+    this->contentState = PAGE_INVALID;
+
+    atomic_store(&this->refCount, 0);
+    atomic_store(&this->isDirty, false);
+
+    mutex_init(&this->pageLock);
+    blockBufferInit(&this->page);
+}
+
+void pageEntryDestroy(PageEntry *this)
+{
+    mutex_destroy(&this->pageLock);
+    blockBufferDestroy(&this->page);
+
+    if (0 != this->refCount) RTFS_LOG(RTFS_LOG_WARNING, "page cache entry(blkoff = %u): refcount = %u while destructed.", this->blkoff, atomic_load(&this->refCount));
+
+    if (0 != this->isDirty) RTFS_LOG(RTFS_LOG_WARNING, "page cache entry(blkoff = %u): still dirty while destructed.", this->blkoff);
+}
+
+pthread_mutex_t *pageEntryGetLock(PageEntry *this)
+{
+    return &this->pageLock;
+}
+
+BlockBuffer *pageEntryGetBuffer(PageEntry *this)
+{
+    return &this->page;
+}
+
+PageState pageEntryGetState(const PageEntry *this)
+{
+    return this->contentState;
+}
+
+void pageEntrySetState(PageEntry *this, PageState state)
+{
+    this->contentState = state;
+}
+
+uint32_t pageEntryGetBlkoff(const PageEntry *this)
+{
+    return this->blkoff;
+}
+
+uint32_t pageEntryGetLpaRef(PageEntry *this)
+{
+    return this->lpa;
+}
+
+void pageEntrySetLpa(PageEntry *this, uint32_t lpa)
+{
+    this->lpa = lpa;
+}
+
+
+bool pageEntryMarkDirty(PageEntry *this)
+{
+    bool expect = false;
+
+
+    return atomic_compare_exchange_strong(&this->isDirty, &expect, true);
+}
---
+++ src/utils/rtfs_multithread.h
@@ -0,0 +1,113 @@
+#ifndef _RTFS_MULTITHREAD_H_
+#define _RTFS_MULTITHREAD_H_
+
+// 用户态、内核多线程同步和互斥工具的兼容层。
+#include <pthread.h>
+
+
+typedef pthread_mutex_t mutex_t;
+typedef pthread_spinlock_t spinlock_t;
+typedef pthread_cond_t cond_t;
+typedef pthread_rwlock_t rwlock_t;
+
+
+__attribute__((unused)) static int mutex_init(mutex_t *self)
+{
+    return pthread_mutex_init(self, NULL);
+}
+
+__attribute__((unused)) static int mutex_lock(mutex_t *self)
+{
+    return pthread_mutex_lock(self);
+}
+
+__attribute__((unused)) static int mutex_trylock(mutex_t *self)
+{
+    return pthread_mutex_trylock(self);
+}
+
+__attribute__((unused)) static int mutex_unlock(mutex_t *self)
+{
+    return pthread_mutex_unlock(self);
+}
+
+__attribute__((unused)) static int mutex_destroy(mutex_t *self)
+{
+    return pthread_mutex_destroy(self);
+}
+
+__attribute__((unused)) static int spin_init(spinlock_t *self)
+{
+    return pthread_spin_init(self, PTHREAD_PROCESS_PRIVATE);
+}
+
+__attribute__((unused)) static int spin_lock(spinlock_t *self)
+{
+    return pthread_spin_lock(self);
+}
+
+__attribute__((unused)) static int spin_unlock(spinlock_t *self)
+{
+    return pthread_spin_unlock(self);
+}
+
+__attribute__((unused)) static int spin_destroy(spinlock_t *self)
+{
+    return pthread_spin_destroy(self);
+}
+
+/* 默认读者优先 */
+__attribute__((unused)) static int rwlock_init(rwlock_t *self)
+{
+    return pthread_rwlock_init(self, NULL);
+}
+
+/* 加共享读锁 */
+__attribute__((unused)) static int rwlock_rdlock(rwlock_t *self)
+{
+    return pthread_rwlock_rdlock(self);
+}
+
+/* 加独占写锁 */
+__attribute__((unused)) static int rwlock_wrlock(rwlock_t *self)
+{
+    return pthread_rwlock_wrlock(self);
+}
+
+__attribute__((unused)) static int rwlock_unlock(rwlock_t *self)
+{
+    return pthread_rwlock_unlock(self);
+}
+
+__attribute__((unused)) static int rwlock_destroy(rwlock_t *self)
+{
+    return pthread_rwlock_destroy(self);
+}
+
+__attribute__((unused)) static int cond_init(cond_t *self)
+{
+    return pthread_cond_init(self, NULL);
+}
+
+__attribute__((unused)) static int cond_wait(cond_t *self, mutex_t *mtx)
+{
+    return pthread_cond_wait(self, mtx);
+}
+
+__attribute__((unused)) static int cond_signal(cond_t *self)
+{
+    return pthread_cond_signal(self);
+}
+
+__attribute__((unused)) static int cond_broadcast(cond_t *self)
+{
+    return pthread_cond_broadcast(self);
+}
+
+__attribute__((unused)) static int cond_destroy(cond_t *self)
+{
+    return pthread_cond_destroy(self);
+}
+
+
+#endif
