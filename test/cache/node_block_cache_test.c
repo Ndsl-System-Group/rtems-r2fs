0b7a88b2aaf8a2678686057cdc6373d44776629b
DavidingPlus <davidingplus@qq.com>
2026-04-27 03:13:30 +0000

    add unit test for NodeBlockCacheEntry

--- test/cache/node_block_cache_test.c
+++ test/cache/node_block_cache_test.c
@@ -2,9 +2,136 @@
 
 #include "cache/node_block_cache.h"
 
+#include <memory.h>
 
-// TODO 因为 BlockBuffer 还没完成，因此涉及到 BlockBuffer 的操作全是空操作，会出现空指针的内存泄露问题导致崩溃，暂时无法测试。
-RTFS_TEST(NbcTest)
+
+RTFS_TEST(NbceInitTest)
 {
-    TEST_PASS();
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    memset(&entry, 0xAA, sizeof(entry));
+
+    nodeBlockCacheEntryInit(&entry, &buffer, 100, 200, 300);
+
+
+    TEST_ASSERT_EQUAL_UINT32(100, entry.nid);
+    TEST_ASSERT_EQUAL_UINT32(200, entry.parentNid);
+    TEST_ASSERT_EQUAL_UINT32(300, entry.lpa);
+    TEST_ASSERT_EQUAL_UINT32(0, entry.refCount);
+    TEST_ASSERT_EQUAL(NODE_BLOCK_CACHE_ENTRY_UPTODATE, entry.state);
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceDestroyTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 10, 20, 30);
+
+    nodeBlockCacheEntryDestroy(&entry);
+
+
+    TEST_ASSERT_EQUAL_UINT32(0, entry.nid);
+    TEST_ASSERT_EQUAL_UINT32(0, entry.parentNid);
+    TEST_ASSERT_EQUAL_UINT32(0, entry.lpa);
+    TEST_ASSERT_EQUAL_UINT32(0, entry.refCount);
+    TEST_ASSERT_EQUAL(NODE_BLOCK_CACHE_ENTRY_DELETED, entry.state);
+
+
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceLpaTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 1, 2, 3);
+
+
+    TEST_ASSERT_EQUAL_UINT32(3, nodeBlockCacheEntryGetLpa(&entry));
+
+    nodeBlockCacheEntrySetLpa(&entry, 999);
+
+    TEST_ASSERT_EQUAL_UINT32(999, nodeBlockCacheEntryGetLpa(&entry));
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceStateTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 1, 2, 3);
+
+
+    TEST_ASSERT_EQUAL(NODE_BLOCK_CACHE_ENTRY_UPTODATE, nodeBlockCacheEntryGetState(&entry));
+
+    nodeBlockCacheEntrySetState(&entry, NODE_BLOCK_CACHE_ENTRY_DIRTY);
+
+    TEST_ASSERT_EQUAL(NODE_BLOCK_CACHE_ENTRY_DIRTY, nodeBlockCacheEntryGetState(&entry));
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceBufferTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 1, 2, 3);
+
+
+    TEST_ASSERT_EQUAL_PTR(&entry.node, nodeBlockCacheEntryGetNodeBuffer(&entry));
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceNidTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 777, 2, 3);
+
+
+    TEST_ASSERT_EQUAL_UINT32(777, nodeBlockCacheEntryGetNid(&entry));
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
+}
+
+RTFS_TEST(NbceNodeBlockPtrTest)
+{
+    BlockBuffer buffer;
+    blockBufferInit(&buffer);
+
+    NodeBlockCacheEntry entry;
+    nodeBlockCacheEntryInit(&entry, &buffer, 1, 2, 3);
+
+
+    TEST_ASSERT_EQUAL_PTR(blockBufferGetPtr(&entry.node), nodeBlockCacheEntryGetNodeBlockPtr(&entry));
+
+
+    nodeBlockCacheEntryDestroy(&entry);
+    blockBufferDestroy(&buffer);
 }
